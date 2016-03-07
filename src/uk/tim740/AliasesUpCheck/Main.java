25be61bcdc3c4cd1a197755da2de66df4c01211e
tim740 <tim740x@gmail.com>
2016-03-07 00:40:59 +0000

    v1.1: Optimise

--- src/uk/tim740/AliasesUpCheck/Main.java
+++ src/uk/tim740/AliasesUpCheck/Main.java
@@ -19,41 +19,38 @@ public class Main extends JavaPlugin {
         verChkTimer();
     }
 
-    public static void verChk() {
-        Bukkit.getServer().getLogger().info("[skAliasesVerCheck] Checking for update now you will be notified if there is an update!");
-        URL url;
+    public void verChk() {
+        getLogger().info("Checking for update now you will be notified if there is an update!");
         String v = "";
         try {
-            url = new URL("https://tim740.github.io/aliasesVer");
+            URL url = new URL("https://tim740.github.io/aliasesVer");
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
             String inLi;
             while ((inLi = in.readLine()) != null)
                 v = inLi;
             in.close();
         } catch (Exception e) {
-            Bukkit.getServer().getLogger().severe(e.getCause().getMessage());
+            getLogger().severe(e.getCause().getMessage());
         }
-        String s = "#! VERSION: " + v +"!";
         String fv = "";
         try {
             BufferedReader br = new BufferedReader(new FileReader("plugins\\Skript\\aliases-english.sk"));
-            fv = br.readLine();
+            fv = br.readLine().replaceAll("#! VERSION: ", "").replaceAll("!", "");
         } catch (Exception e) {
-            Bukkit.getServer().getLogger().severe(e.getCause().getMessage());
+            getLogger().severe(e.getCause().getMessage());
         }
-        if (!Objects.equals(fv, s)){
-            String av = fv.replaceAll("#! VERSION: ", "").replaceAll("!", "");
-            Bukkit.getServer().getLogger().warning("[skAliasesVerCheck] A new version of the aliases is out v" + v + " (Currently using v" + av + ")");
-            Bukkit.getServer().getLogger().warning("[skAliasesVerCheck] You can find the latest version here: https://forums.skunity.com/t/latest-aliases-v1-9-id-order-full-1-9-support/40");
+        if (!Objects.equals(fv, v)){
+            getLogger().warning("A new version of the aliases is out v" + v + " (Currently using v" + fv + ")");
+            getLogger().warning("You can find the latest version here: https://forums.skunity.com/t/latest-aliases-v1-9-id-order-full-1-9-support/40");
 
-            Bukkit.broadcast("[skAliasesVerCheck] A new version of the aliases is out v" + v + " (Currently using v" + av + ")", "skAliasesVerCheck.notice");
+            Bukkit.broadcast("[skAliasesVerCheck] A new version of the aliases is out v" + v + " (Currently using v" + fv + ")", "skAliasesVerCheck.notice");
             Bukkit.broadcast("[skAliasesVerCheck] You can find the latest version here: https://forums.skunity.com/t/latest-aliases-v1-9-id-order-full-1-9-support/40", "skAliasesVerCheck.notice");
         }else{
-            Bukkit.getServer().getLogger().info("[skAliasesVerCheck] It seems like your using the latest version!");
+            getLogger().info("It seems like your using the latest version!");
         }
     }
 
-    public static void verChkTimer() {
+    public void verChkTimer() {
         Thread t = new Thread() {
             @SuppressWarnings("InfiniteLoopStatement")
             @Override
@@ -63,7 +60,7 @@ public class Main extends JavaPlugin {
                         verChk();
                         Thread.sleep(1000*60*60*12);
                     } catch (InterruptedException e) {
-                        Bukkit.getServer().getLogger().severe(e.getCause().getMessage());
+                        getLogger().severe(e.getCause().getMessage());
                     }
                 }
             }
